bf52fb1d3c34ca8dab7a992ee28194a385e29012
admire <admiremurombedzi8888@gmail.com>
2025-11-03 17:23:23 +0200

    Delete WDD131 directory

--- WDD131/scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Dynamically insert the current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Display the last modified date
-document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
