7229e428ee9f2e1b0dd5f96b3c8029979751fabb
admire <admiremurombedzi8888@gmail.com>
2025-11-04 13:17:53 +0200

    Delete scripts directory

--- scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-// Insert the current year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-// Insert the last modified date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
-
